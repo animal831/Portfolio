88fd5c30aa0474a4b937dbc16633d8021c2aee7c
sanju <bm.madhubalakrishna@gmail.com>
2024-01-05 06:55:16 +0000

    finished

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu(){
+const menu = document.querySelector(".menu_links");
+const icon = document.querySelector(".hamburger_icon");
+
+menu.classList.toggle("open");
+icon.classList.toggle("open");
+
+}
